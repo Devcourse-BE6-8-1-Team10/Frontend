6e312fcedae6708f4a719ecdb9377a58d1d16749
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-07-16 11:53:42 +0900

    feat: footer 추가

---
+++ components/common/Footer.tsx
@@ -0,0 +1,8 @@
+// components/Footer.tsx
+export default function Footer() {
+  return (
+    <div className="w-full h-14 bg-[#6e7780] flex justify-center items-center text-white text-base fixed bottom-0 left-0 z-[100] py-4">
+      Devcourse-BE6-8-1-Team10 @ 2025. All rights reserved.
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header/>
-        <main className="max-w-7xl mx-auto">
-            {children}
-        </main>
+        <Header />
+        <main className="max-w-7xl mx-auto">{children}</main>
+        <Footer />
       </body>
     </html>
   );
